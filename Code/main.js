0023eccb4b09fd3bf3dfe23cefa4fc70c08a7793
nicco <nicco.borgioli@gmail.com>
2017-06-14 12:36:49 +0200

    14.06.2017

--- Code/main.js
+++ Code/main.js
@@ -1,21 +1,37 @@
 'use strict';
 
-let bodyElement, navElement, scrollElements, classes
+let bodyElement, navElement, scrollElements, classes, dropdownElement, open = false
 
 function check(event) {
-	if (bodyElement.scrollTop > 0)
-		navElement.classList = classes.join(' ')
+	if (bodyElement.scrollTop > 0) {
+		// navElement[0].classList = classes.join(' ')
+		// navElement[1].classList = classes.join(' ')
+		navElement.forEach(e => e.classList = classes.join(' '))
+
+	} else {
+		// navElement[0].classList = classes.slice(0, 2).join(' ')
+		// navElement[1].classList = classes.slice(0, 2).join(' ')
+		navElement.forEach(e => e.classList = classes.slice(0, 2).join(' '))
+	}
+}
+
+function toggle(e) {
+	if (open)
+		dropdownElement.classList.add('hidden')
 	else
-		navElement.classList = classes.slice(0, 2).join(' ')
+		dropdownElement.classList.remove('hidden')
+	open = !open
 }
 
 function init() {
 	bodyElement = document.querySelector('body')
-	navElement = document.querySelector('.navbar')
+	dropdownElement = document.querySelector('.dropdown')
+	navElement = document.querySelectorAll('.navbar')
 	scrollElements = document.querySelectorAll('*[data-onscroll]')
 	classes = ['navbar', 'center', 'navbar-full']
 
 	bodyElement.onscroll = check
+	document.querySelector('#navbar-hamburger').addEventListener('click', toggle)
 }
 
 window.onload = init;
