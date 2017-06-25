22ce1b823d3ffcbe2ae22db9ae116e324de9303e
nicco <nicco.borgioli@gmail.com>
2017-06-25 15:10:38 +0200

    Docker Composer File

---
+++ Backend/code/index.php
@@ -0,0 +1,18 @@
+<?php
+
+// echo phpinfo();
+
+$servername = "localhost";
+$username = "root";
+$password = "nope";
+
+try {
+	$conn = new PDO("mysql:host=maria;dbname=db", $username, $password);
+	// set the PDO error mode to exception
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	echo "Connected successfully"; 
+}
+catch(PDOException $e)
+{
+	echo "Connection failed: " . $e->getMessage();
+}
